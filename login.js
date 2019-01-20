87b3f14da75e9691d4eb36a0f1c7ba3002eccf76
nagared <nagared>
2019-01-20 12:11:42 +0530

    Try1

--- login.js
+++ login.js
@@ -73,83 +73,83 @@
                                     style : styles.loginButton,
                                     // ----------
 				                                 onPress: function(){
-                                           if (componentState.state.userName === '' || componentState.state.userName == undefined) {
-                                                NativeBase.Toast.show({
-                                                    text: 'Please enter Username',
-                                                    position: 'bottom',
-                                                    buttonText: 'Okay',
-                                                    duration: 5000,
-                                                    type: 'danger',
-                                                })
-                                            }
-                                            else if (componentState.state.password === '' || componentState.state.password == undefined) {
-                                                NativeBase.Toast.show({
-                                                    text: 'Please enter Password',
-                                                    position: 'bottom',
-                                                    buttonText: 'Okay',
-                                                    duration: 5000,
-                                                    type: 'danger',
-                                                })
-                                            }
-                                            /*else if (!this.isValidPassword(this.state.password) || this.state.password.length <= 8) {
-                                                Toast.show({
-                                                    text: "Password should have minimum 8 characters with 1 numeric and 1 alphabet",
-                                                    position: 'bottom',
-                                                    buttonText: 'Okay',
-                                                    duration: 5000,
-                                                    type: 'danger',
-                                                })
-                                            } */
-                                            else {
-                                                componentState.setState({progressModal:true});
-                                                return(<Overlay
-                                                    visible={componentState.state.progressModal}
-                                                            //closeOnTouchOutside
-                                                             animationType="zoomIn"
-                                                             containerStyle={{backgroundColor: 'rgba(37, 8, 10, 0.78)'}}
-                                                             childrenWrapperStyle={{backgroundColor: '#eee'}}
-                                                             animationDuration={100}>
-                                                             <View>
-                                                                 <ActivityIndicator size="large" color="#3491cc"/>
-                                                                <Text>Please wait a moment...</Text>
-                                                            </View>
-                                                       </Overlay>,function() {
-                                                        let userJsonData = { "loginName": componentState.state.userName, "password": componentState.state.password }
-                                                        fetch('https://cfsfiserv.com/QEUATSMT/api/Authentication/LogIn',
-                                                            {
-                                                                method: 'POST',
-                                                                headers: {
-                                                                    'Content-Type': 'application/json',
-                                                                },
-                                                                body: JSON.stringify(userJsonData),
-                                                            }).then(response => {
-                                                                componentState.setState({progressModal:false});
-                                                                var responseObj = JSON.parse(response._bodyText);
-                                                                var TokenResponse = responseObj.antiForgeryToken;
-                                                                //console.log("responseObj  =::" + responseObj.antiForgeryToken);
-                                                                if (TokenResponse == '' || TokenResponse == undefined) {
-                                                                    componentState.setState({progressModal:false});
-                                                                    Alert.alert(
-                                                                        '',
-                                                                        'Please enter the valid UserName and Password',
-                                                                        [
-                                                                            { text: '', onPress: () => console.log('Ask me later pressed') },
-                                                                            { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
-                                                                            { text: 'OK', onPress: () => console.log('OK Pressed') },
-                                                                        ],
-                                                                        { cancelable: false }
-                                                                    )
-                                                                } else {
-                                                                    componentState.setState({progressModal:false});
-                                                                    componentState.props.navigation.navigate("AccountSummary", {
-                                                                        token: TokenResponse,
-                                                                    });
-                                                                }
-                                                            });
-                                                       });
-                                                //this.setState({requestLoading:true});
-                                                //this.loginCall();
-                                            }
+                                           // if (componentState.state.userName === '' || componentState.state.userName == undefined) {
+                                           //      NativeBase.Toast.show({
+                                           //          text: 'Please enter Username',
+                                           //          position: 'bottom',
+                                           //          buttonText: 'Okay',
+                                           //          duration: 5000,
+                                           //          type: 'danger',
+                                           //      })
+                                           //  }
+                                           //  else if (componentState.state.password === '' || componentState.state.password == undefined) {
+                                           //      NativeBase.Toast.show({
+                                           //          text: 'Please enter Password',
+                                           //          position: 'bottom',
+                                           //          buttonText: 'Okay',
+                                           //          duration: 5000,
+                                           //          type: 'danger',
+                                           //      })
+                                           //  }
+                                           //  /*else if (!this.isValidPassword(this.state.password) || this.state.password.length <= 8) {
+                                           //      Toast.show({
+                                           //          text: "Password should have minimum 8 characters with 1 numeric and 1 alphabet",
+                                           //          position: 'bottom',
+                                           //          buttonText: 'Okay',
+                                           //          duration: 5000,
+                                           //          type: 'danger',
+                                           //      })
+                                           //  } */
+                                           //  else {
+                                           //      componentState.setState({progressModal:true});
+                                           //      return(<Overlay
+                                           //          visible={componentState.state.progressModal}
+                                           //                  //closeOnTouchOutside
+                                           //                   animationType="zoomIn"
+                                           //                   containerStyle={{backgroundColor: 'rgba(37, 8, 10, 0.78)'}}
+                                           //                   childrenWrapperStyle={{backgroundColor: '#eee'}}
+                                           //                   animationDuration={100}>
+                                           //                   <View>
+                                           //                       <ActivityIndicator size="large" color="#3491cc"/>
+                                           //                      <Text>Please wait a moment...</Text>
+                                           //                  </View>
+                                           //             </Overlay>,function() {
+                                           //              let userJsonData = { "loginName": componentState.state.userName, "password": componentState.state.password }
+                                           //              fetch('https://cfsfiserv.com/QEUATSMT/api/Authentication/LogIn',
+                                           //                  {
+                                           //                      method: 'POST',
+                                           //                      headers: {
+                                           //                          'Content-Type': 'application/json',
+                                           //                      },
+                                           //                      body: JSON.stringify(userJsonData),
+                                           //                  }).then(response => {
+                                           //                      componentState.setState({progressModal:false});
+                                           //                      var responseObj = JSON.parse(response._bodyText);
+                                           //                      var TokenResponse = responseObj.antiForgeryToken;
+                                           //                      //console.log("responseObj  =::" + responseObj.antiForgeryToken);
+                                           //                      if (TokenResponse == '' || TokenResponse == undefined) {
+                                           //                          componentState.setState({progressModal:false});
+                                           //                          Alert.alert(
+                                           //                              '',
+                                           //                              'Please enter the valid UserName and Password',
+                                           //                              [
+                                           //                                  { text: '', onPress: () => console.log('Ask me later pressed') },
+                                           //                                  { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+                                           //                                  { text: 'OK', onPress: () => console.log('OK Pressed') },
+                                           //                              ],
+                                           //                              { cancelable: false }
+                                           //                          )
+                                           //                      } else {
+                                           //                          componentState.setState({progressModal:false});
+                                           //                          componentState.props.navigation.navigate("AccountSummary", {
+                                           //                              token: TokenResponse,
+                                           //                          });
+                                           //                      }
+                                           //                  });
+                                           //             });
+                                           //      //this.setState({requestLoading:true});
+                                           //      //this.loginCall();
+                                           //  }
         // ---------
                                     }
                                 },[react_1.createElement(_reactNative.Text, {
